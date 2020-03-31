d7cf4af1867c5136155878bf3b46a4d8280ebd7d
Velten <velten.heyn_ext@scout24.com>
2020-03-31 17:53:25 +0200

    read yaml path from argv

--- cli.js
+++ cli.js
@@ -1,12 +1,12 @@
-#!/usr/bin/env
+#!/usr/bin/env node
 
 const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = YAML.load(process.argv[2]);
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
