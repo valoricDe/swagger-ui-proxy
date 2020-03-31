f9bd236b232491a02d16bc8e98524f39d28b2b46
Velten <velten.heyn_ext@scout24.com>
2020-03-31 18:25:21 +0200

    improvements

--- cli.js
+++ cli.js
@@ -8,9 +8,10 @@ const corsAnywhere = require('cors-anywhere')
 
 const swaggerDocument = YAML.load(process.argv[2]);
 const PORT = process.env.PORT || 3001;
+const origin = `localhost:${PORT}`
 
 let proxy = corsAnywhere.createServer({
-  originWhitelist: [`localhost:${PORT}`], // Allow all origins
+  originWhitelist: [origin], // Allow all origins
   requireHeaders: [], // Do not require any headers.
   removeHeaders: [] // Do not remove any headers.
 });
@@ -23,4 +24,21 @@ app.get('/proxy/:proxyUrl*', (req, res) => {
 
 app.use(swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`)
+
+  if(process.argv.indexOf('--open')) {
+    const { exec } = require("child_process");
+    exec(`open ${origin}`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+  }
+});
