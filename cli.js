a5963d20d8511bc9ff619af3523a4cd8a97510f9
Velten <velten.heyn_ext@scout24.com>
2020-03-31 18:11:37 +0200

    improvements

--- cli.js
+++ cli.js
@@ -4,14 +4,23 @@ const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
-const swaggerDocument = YAML.load(process.argv[2]);
+const corsAnywhere = require('cors-anywhere')
 
+const swaggerDocument = YAML.load(process.argv[2]);
 const PORT = process.env.PORT || 3001;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", `http://localhost:${PORT}`);
-  next();
+let proxy = corsAnywhere.createServer({
+  originWhitelist: [`localhost:${PORT}`], // Allow all origins
+  requireHeaders: [], // Do not require any headers.
+  removeHeaders: [] // Do not remove any headers.
 });
+
+/* Attach our cors proxy to the existing API on the /proxy endpoint. */
+api.get('/proxy/:proxyUrl*', (req, res) => {
+  req.url = req.url.replace('/proxy/', '/'); // Strip '/proxy' from the front of the URL, else the proxy won't work.
+  proxy.emit('request', req, res);
+});
+
 app.use(swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
