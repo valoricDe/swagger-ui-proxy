639e2e2375b60927db92fcbcad8ec35ca97d0775
Velten <velten.heyn_ext@scout24.com>
2020-03-31 18:00:52 +0200

    improvements

--- cli.js
+++ cli.js
@@ -6,7 +6,12 @@ const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load(process.argv[2]);
 
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
 const PORT = process.env.PORT || 3001;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", `http://localhost:${PORT}`);
+  next();
+});
+app.use(swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
